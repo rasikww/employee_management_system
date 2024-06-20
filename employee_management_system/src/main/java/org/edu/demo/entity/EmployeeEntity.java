fe555405adc9344189c6a1d027ef243fe55cbc3e
rasikww <rasikauom@gmail.com>
2024-06-20 21:48:25 +0530

    find an employee by id API completed.

--- employee_management_system/src/main/java/org/edu/demo/entity/EmployeeEntity.java
+++ employee_management_system/src/main/java/org/edu/demo/entity/EmployeeEntity.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "employee")
 public class EmployeeEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
